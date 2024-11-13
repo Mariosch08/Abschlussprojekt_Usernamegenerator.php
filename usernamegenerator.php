577d21c894eb9ae9ef9e55b59bfeea1d8f517f67
Mariosch08 <mario.schwyzer@gmail.com>
2024-11-13 16:19:27 +0100

    abschluss PHP

---
+++ usernamegenerator.php
@@ -0,0 +1,145 @@
+<?php
+
+//array für adjektiv Teil des Usernames erstllt
+$adjektive = array(
+    "snarling", "blaring", "haunting", "cheery", "zesty", "meek", "concerned", "sticky", "sordid", "brilliant",
+    "antsy", "dual", "scientific", "assured", "honored", "fair", "scrawny", "ancient", "sassy", "blushing",
+    "beneficial", "greasy", "guarded", "super", "abiding", "romantic", "big", "ad hoc", "teeny-tiny", "successful",
+    "fertile", "quirky", "moldy", "insubstantial", "ruddy", "greedy", "humdrum", "taboo", "knowledgeable", "grotesque",
+    "sturdy", "starry", "two", "energetic", "incomplete", "apprehensive", "curvy", "lasting", "French", "luxuriant",
+    "glossy", "ethical", "aquatic", "bustling", "thoughtful", "giddy", "quaint", "secret", "orange", "marked",
+    "quizzical", "vacant", "qualified", "craven", "grizzled", "ghastly", "upset", "mean", "oblivious", "luxurious",
+    "pushy", "elementary", "extroverted", "wakeful", "rabid", "clean", "harmful", "ugly", "ceaseless", "gaudy",
+    "skinny", "juicy", "required", "everlasting", "costly", "chivalrous", "kooky", "glorious", "truthful", "classic",
+    "conscious", "flustered", "tightfisted", "earsplitting", "zonked", "scandalous", "safe", "elaborate", "hanging",
+    "pessimistic", "vengeful", "possessive", "sable", "tasteless", "intrigued", "measly", "crafty", "minty", "smelly",
+    "numerous", "ubiquitous", "imaginary", "belligerent", "smarmy", "awful", "silent", "endurable", "fatal", "harmless",
+    "burdensome", "faded", "bossy", "tranquil", "evasive", "several", "squiggly", "live", "edible", "fast", "zigzag",
+    "impassioned", "eager", "abortive", "whispered", "eminent", "obedient", "grounded", "potable", "worthy", "rotund",
+    "black", "narrow", "steadfast", "criminal", "level", "combative", "adamant", "handy", "parsimonious", "determined",
+    "zippy", "jubilant", "depressed", "golden", "six", "subdued", "convoluted", "arctic", "ornate", "untrue", "burly",
+    "overlooked", "perfect", "clueless", "illegal", "tangy", "versed", "sandy", "dependent", "quintessential", "annoyed",
+    "variable", "puzzled", "frightening", "limp", "symptomatic", "dapper", "famous", "victorious", "lustrous", "gainful",
+    "well-worn", "stupendous", "white", "portly", "fanatical", "bowed", "changeable", "wet", "living", "shaky", "motherly",
+    "terrible", "decorous", "tearful", "helpless", "tangible", "favorable", "strict", "squeaky", "slim", "petty", "brief",
+    "extraneous", "flashy", "organic", "rotating", "moist", "alarmed", "scratchy", "doubtful", "low", "equable", "rich",
+    "mighty", "trifling", "grateful", "euphoric", "bloody", "illustrious", "grubby", "accidental", "voracious", "left",
+    "grotesque", "weak", "impolite", "cute", "stark", "insistent", "outgoing", "whopping", "growing", "bronze", "foolhardy",
+    "incompatible", "favorite", "smug", "guiltless", "nonsensical", "emotional", "lonely", "better", "biodegradable",
+    "flawless", "tough", "devoted", "acrid", "ossified", "faraway", "regal", "weighty", "spectacular", "powerless",
+    "chunky", "medical", "fierce", "feminine", "cowardly", "sleepy", "drafty", "abhorrent", "frigid", "stereotyped",
+    "united", "languid", "keen", "querulous", "frequent", "mysterious", "unsung", "impish", "earthy", "sociable",
+    "adolescent", "fat", "considerate", "average", "spicy", "limp", "young", "alcoholic", "traumatic", "rhetorical",
+    "striking", "idealistic", "rusty", "abrasive", "obsolete", "attached", "living", "elegant", "unrealistic", "tedious",
+    "blond", "hilarious", "gabby", "messy", "pesky", "divergent", "small", "tart", "determined", "illiterate", "succulent",
+    "good-natured", "frayed", "exhilarated", "married", "windy", "immaterial", "idolized", "original", "unwritten",
+    "misguided", "optimal", "lovable", "responsible", "grieving", "stable", "staking", "ready", "dashing", "wide-eyed",
+    "damaged", "vital", "ambiguous", "quaint", "superb", "curly", "horrific", "sulky", "skillful", "macabre", "unarmed",
+    "excellent", "stingy", "halting", "stiff", "sassy", "tremendous", "vacuous", "calculating", "repulsive", "bulky",
+    "traumatic", "limited", "ripe", "bored", "puzzled", "scattered", "malicious", "shocking", "elegant", "belligerent",
+    "cluttered", "prize", "crowded", "premium", "oafish", "rotten", "unique", "traumatic", "nine", "wavy", "impish",
+    "cavernous", "bouncy", "ahead", "waggish", "obese", "identical", "right", "aware", "bogus", "wet", "limping",
+    "heartbreaking", "pink", "disagreeable", "admirable", "hurried", "disgusted", "two", "bewitched", "zany", "better",
+    "inborn", "quaint", "melancholy", "lucky", "smiling", "sweltering", "open", "loyal", "mundane", "befitting",
+    "zealous", "dead", "grandiose", "yawning", "cooing", "swanky", "shameful", "ornate", "imaginative", "proper",
+    "lazy", "puny", "annual", "canine", "ossified", "grand", "spiritual", "classic", "modern", "distinct", "long",
+    "quirky", "tough", "special", "magnificent", "mighty", "adorable", "bold", "realistic", "simple", "fantastic",
+    "grandiose", "irreplaceable", "hopeful"
+);
+//array für Nomen Teil des Usernames erstellt
+$nomen = array(
+    "frog", "sound", "rose", "self", "thunder", "flock", "trousers", "yard", "structure", "event",
+    "expansion", "trip", "crime", "mouth", "mother", "science", "dress", "swing", "loaf", "cable",
+    "current", "vase", "farmer", "adjustment", "tent", "judge", "boys", "beginner", "sea", "statement",
+    "size", "low", "pickle", "waste", "measure", "salt", "market", "wish", "lizards", "tomatoes",
+    "team", "sort", "airplane", "hot", "fly", "arm", "offer", "way", "coat", "title", "cemetery",
+    "hospital", "roof", "brain", "prose", "mailbox", "poison", "yarn", "paint", "knee", "mark",
+    "creature", "picture", "mass", "apples", "ear", "interest", "friends", "caption", "pump", "clover",
+    "hate", "feeling", "government", "force", "rake", "sheep", "hammer", "sign", "toad", "kitten",
+    "ball", "rabbit", "route", "pencil", "manager", "time", "ocean", "leaf", "trucks", "wire",
+    "giants", "cushion", "road", "straw", "things", "sister", "wash", "balls", "square", "record",
+    "nose", "attack", "geese", "rainstorm", "flame", "growth", "bikes", "pie", "driving", "ship",
+    "look", "kitty", "line", "guitar", "animal", "drug", "education", "robin", "duck", "beast",
+    "crib", "tank", "flavor", "work", "copper", "swim", "cub", "tree", "star", "gate", "thumb",
+    "thing", "tongue", "window", "berry", "cork", "building", "curve", "stretch", "story", "journey",
+    "color", "lead", "number", "receipt", "soap", "society", "books", "popcorn", "glass", "disgust",
+    "muscle", "order", "plot", "sheet", "bears", "egg", "motion", "camp", "addition", "health",
+    "news", "train", "jump", "pigs", "deer", "arithmetic", "use", "horses", "industry", "toothbrush",
+    "credit", "blade", "crayon", "dog", "sneeze", "play", "produce", "fold", "weather", "middle",
+    "join", "beds", "grain", "battle", "joke", "marble", "band", "veil", "point", "planes", "wool",
+    "plantation", "honey", "girls", "step", "cover", "believe", "need", "lamp", "eyes", "jeans",
+    "doll", "basin", "scarf", "heart", "horn", "afterthought", "rate", "spade", "relation", "string",
+    "trees", "theory", "garden", "experience", "drawer", "start", "secretary", "town", "feast", "sail",
+    "design", "rice", "idea", "pin", "shape", "grip", "history", "comb", "creator", "goat", "wind",
+    "hope", "cream", "skate", "cough", "scale", "stop", "discovery", "earthquake", "cow", "power",
+    "love", "snakes", "net", "cannon", "treatment", "agreement", "belief", "rhythm", "oven", "detail",
+    "laugh", "hair", "leather", "toothpaste", "bike", "porter", "lip", "zoo", "move", "bedroom",
+    "back", "toys", "tin", "parent", "quiet", "reaction", "party", "boats", "quarter", "death", "yam",
+    "seed", "pear", "punishment", "observation", "property", "comfort", "bomb", "hen", "cent", "church",
+    "turkey", "quince", "sponge", "brass", "apparatus", "summer", "chicken", "cobweb", "beam", "teeth",
+    "meeting", "addition", "zinc", "print", "pull", "kite", "increase", "discussion", "bushes", "army",
+    "wine", "mind", "language", "goose", "cart", "chairs", "pain", "dinner", "word", "fruit", "meal",
+    "family", "spot", "moon", "mountain", "laugh", "pet", "partner", "process", "seat", "frogs", "skin",
+    "level", "sense", "limit", "writer", "bead", "iron", "committee", "cherries", "pan", "force",
+    "representative", "wren", "space", "reward", "worm", "minute", "hand", "flag", "cakes", "neck",
+    "care", "attention", "humor", "ornament", "attraction", "request", "digestion", "trouble", "smash",
+    "badge", "dad", "memory", "truck", "sand", "leg", "shock", "name", "father", "glue", "shop",
+    "distribution", "price", "aunt", "show", "earth", "gate", "light", "tail", "ladybug", "good-bye",
+    "governor", "banana", "cellar", "thought", "screw", "coat", "brush", "box", "act", "lift",
+    "support", "country", "winter", "store", "fairies", "coil", "balance", "war", "nerve", "clover",
+    "copper", "pies", "sock", "reason", "fuel", "learning", "door", "market", "spiders", "coast",
+    "test", "flower", "baseball", "appliance", "pocket", "border", "thread", "harmony", "question",
+    "bird", "scarecrow", "ticket", "dinosaurs", "clam", "purpose", "bulb", "unit", "notebook", "stamp",
+    "system", "crowd", "expert", "skirt", "magic", "rock", "dime", "walk", "girl", "page", "company",
+    "bit", "shelf", "amusement", "eggs", "answer", "development", "room", "oil", "temper", "bath",
+    "airplane", "library", "wax", "cats", "hall", "ant", "music", "wing", "vest", "food", "quarter",
+    "grade", "reaction", "lock", "hobbies", "cheese", "wave", "account", "prison", "error", "airport",
+    "body", "donkey", "taste", "match", "songs", "canvas", "fight", "behavior", "flame", "quiver",
+    "trucks", "canvas", "lunch", "clock", "bat", "wound", "books", "street", "push", "selection",
+    "territory", "cup", "card", "division", "destruction", "haircut", "trick", "tail", "island",
+    "light", "creature", "stamp", "stream", "group", "touch", "religion", "change", "yarn", "furniture",
+    "sticks", "thread", "liquid", "pollution", "sleep", "stranger", "string", "hot", "bottle", "control",
+    "ants", "cattle", "mint", "stove", "toy", "jar", "faucet", "hall", "advertisement", "support", "screw",
+    "thread", "chairs", "reaction", "party"
+);
+//function für überprüfung des Usernames erstllet
+function usernamevalid($username)
+{
+        // Überprüft, ob der Username mindestens eine Zahl enthält und nur Buchstaben und Zahlen verwendet.
+    if (!preg_match("/[0-9]/", $username)) {
+        echo("Der Username muss mindestens eine Zahl enthalten.\n");
+        return false;
+    }
+    return true;
+}
+//Schleife für abfrage des Gewünschten Usernamens
+while (true) {
+    $answerq1 = readline("\nMöchten sie ihren Username Selbst erstellen? (Y/N)");
+    if ($answerq1 == "N") {
+        //befehle für zufälliges auswählen von den Wörtern aus den arrays
+        $rand_keysn = array_rand($nomen);
+        $usernamep1 = ucfirst($nomen[$rand_keysn]);
+
+        $rand_keysa = array_rand($adjektive);
+        $usernamep2 = ucfirst($adjektive[$rand_keysa]);
+        $numberrand = rand(0, 10000);
+        $username = "$usernamep2$usernamep1$numberrand\n";
+        echo("Dein Username lautet:     $username\n");
+    } elseif ($answerq1 == "Y") {
+        //Username selbst erstellen
+        $username = readline("Wie sollte dein Username lauten?");
+        if (usernamevalid($username)) {
+            echo("Dein Username lautet:     $username\n");
+            break;
+        } else {
+            echo "Ungültiger Benutzername. Bitte versuche sie es erneut.";
+        }
+    } else {
+        echo "ungültige Eingabe. Nur Y oder N eingeben.";
+    };
+};
+//befehl für das Anzeigen des Usernamens
+$input = readline("");
+if ($input == "Username") {
+    echo "Dein Username lautet: $username\n";
+};
